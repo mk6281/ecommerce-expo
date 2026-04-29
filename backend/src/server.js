3ba6121b2ccb2300cd7cfd9df894441a7e4f623b
mk6281 <manikantaperneedi000@gmail.com>
2026-04-29 11:19:54 +0530

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/api/health', (req, res) => {
+    res.status(200).json({ message: 'Server is running!' });
+});
+
+app.listen(3000, () => console.log('Server running on port 3000'));
