bc69ea6bf5556aeafc64779a64142fd198466c80
mk6281 <manikantaperneedi000@gmail.com>
2026-05-03 13:00:42 +0530

    refactor: restructure backend package and add environment configuration
    - Updated package.json to rename project to "admin", set version to "0.0.0", and added new dependencies for Clerk, Sentry, Tailwind CSS, and React.
    - Introduced a new env.js file to manage environment variables using dotenv.
    - Modified server.js to serve static files in production and handle routing for the admin app.
    - Added favicon.svg as a new asset.
    - Created a new root package.json for the e-comm-app to manage backend and admin builds and starts.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,26 @@
 import express from 'express';
+import path from "path";
 
-const app = express();
+import { ENV } from "./config/env.js";
 
+
+
+const __dirname = path.resolve();
+
+const app = express();  
 app.get('/api/health', (req, res) => {
     res.status(200).json({ message: 'Server is running!' });
 });
+
+
+// make our app ready for deployment
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../admin/dist")));
+
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../admin", "dist", "index.html"));
+  });
+}
+
 
 app.listen(3000, () => console.log('Server running on port 3000'));
