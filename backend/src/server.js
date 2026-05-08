e8a57ee06185c0c64a6a8eba1718d417969b615e
mk6281 <manikantaperneedi000@gmail.com>
2026-05-08 12:58:20 +0530

    feat: Add CORS support and enable environment variables in server configuration

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,7 @@ import path from "path";
 import { connectDB } from "./config/db.js";
 import { clerkMiddleware } from '@clerk/express'
 import { serve } from "inngest/express";
-// import cors from "cors";
+import cors from "cors";
 
 import { functions, inngest } from "./config/inngest.js";
 
@@ -13,11 +13,11 @@ import { ENV } from "./config/env.js";
 
 
 import adminRoutes from "./routes/admin.route.js";
-// import userRoutes from "./routes/user.route.js";
-// import orderRoutes from "./routes/order.route.js";
-// import reviewRoutes from "./routes/review.route.js";
-// import productRoutes from "./routes/product.route.js";
-// import cartRoutes from "./routes/cart.route.js";
+import userRoutes from "./routes/user.route.js";
+import orderRoutes from "./routes/order.route.js";
+import reviewRoutes from "./routes/review.route.js";
+import productRoutes from "./routes/product.route.js";
+import cartRoutes from "./routes/cart.route.js";
 // import paymentRoutes from "./routes/payment.route.js";
 
 
@@ -28,6 +28,8 @@ const __dirname = path.resolve();
 const app = express(); 
 app.use(express.json());
 app.use(clerkMiddleware());
+
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true })); // credentials: true allows the browser to send the cookies to the server with the request
  
 app.get('/api/health', (req, res) => {
     res.status(200).json({ message: 'Server is running!' });
@@ -48,11 +50,11 @@ if (ENV.NODE_ENV === "production") {
 
 
 app.use("/api/admin", adminRoutes);
-// app.use("/api/users", userRoutes);
-// app.use("/api/orders", orderRoutes);
-// app.use("/api/reviews", reviewRoutes);
-// app.use("/api/products", productRoutes);
-// app.use("/api/cart", cartRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/reviews", reviewRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
 
 
 
